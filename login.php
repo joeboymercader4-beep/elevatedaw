c0b4d164ae163c8d65c873a7646b16dd114a5a1c
joeboymercader4-beep <joeboymercader4@gmail.com>
2026-08-19 20:07:36 +0800

    Update login.php

--- login.php
+++ login.php
@@ -17,22 +17,3 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Login</title>
-</head>
-<body>
-    <h2>User Login</h2>
-    <form method="POST" action="login.php">
-        <label>Username:</label><br>
-        <input type="text" name="username" required><br><br>
-        <label>Password:</label><br>
-        <input type="password" name="password" required><br><br>
-        <button type="submit">Login</button>
-    </form>
-
-    <p><?php echo $message; ?></p>
-</body>
-</html>
